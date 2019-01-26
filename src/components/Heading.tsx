12ce185afb6913343ddd3cfb04b1945b6042ec50
Danny <danny@nulleffort.com>
2019-01-26 14:26:48 -0500

    Fix headshot image src.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { imageExtension } from "../utils/client";
 
 const profileImageBase: string = "https://res.cloudinary.com/nulleffort/image/upload/v1546188503/nulleffort/self/Danny2017";
-const headshotRegular: string = profileImageBase.concat(imageExtension);
 const headshotLarge: string = profileImageBase.concat("_2x").concat(imageExtension);
 
 const Heading = (): JSX.Element => (
@@ -11,8 +10,7 @@ const Heading = (): JSX.Element => (
 		<p>Full Stack Developer</p>
 		<img
 			className="avatar"
-			src={headshotRegular}
-			srcSet={`${headshotRegular} 1x, ${headshotLarge} 2x`}
+			src={headshotLarge}
 			alt="Danny headshot"
 		/>
 	</div>
