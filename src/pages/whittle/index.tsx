8673fb04dd1c034a66f1b03b11b18f3bae83c95f
DannyHinshaw <danny@nulleffort.com>
2019-11-17 06:30:42 -0500

    Security patches.

--- src/pages/whittle/index.tsx
+++ src/pages/whittle/index.tsx
@@ -55,9 +55,6 @@ const WhittlePage = (): JSX.Element => {
 
 			<footer>
 				<h5 style={{ textAlign: "center" }}>
-					Hide/Show Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
-					href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-					<br />
 					PocketKnife Icon made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good
 					Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
 				</h5>
