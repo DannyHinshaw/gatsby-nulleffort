00ce022e4ea7de74e74563bbacdf1a65acf3f5d7
DannyHinshaw <danny@nulleffort.com>
2020-01-18 06:54:01 -0500

    Add Charlotte to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,12 @@ import React from "react";
 
 const Footer = (): JSX.Element => (
 	<footer className="panel grey-bg">
+		<img
+			id="mainLogo"
+			style={{ borderRadius: "50%", maxWidth: "25vw" }}
+			src="https://res.cloudinary.com/nulleffort/image/upload/v1579345784/nulleffort/self/hat-face-baby.jpg"
+			alt="Pibble Patrol" />
+
 		<div className="quote-container">
 			<div className="quote">
 				<blockquote>
